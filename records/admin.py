b785ef453f516434eb6c9cc18deedba21f0da4f2
Anders Miller <anders.miller@gmail.com>
2024-03-08 16:08:45 +0100

    Change colors in css, made first record

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Records, Category
 
 # Register your models here.
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Records, RecordAdmin)
+admin.site.register(Category, CategoryAdmin)
--- records/views.py
+++ records/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Records
 
-# Create your views here.
+
+def all_records(request):
+    """View for all records"""
+
+    records = Records.objects.all()
+
+    context = {
+        'records': records,
+    }
+
+    return render(request, 'records/records.html', context)
