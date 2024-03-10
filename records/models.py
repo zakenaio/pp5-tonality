72393b0f9f8ceea6ac7bd00df9a74323ea523fdd
Anders Miller <anders.miller@gmail.com>
2024-03-10 13:35:50 +0100

    Made categories views work

--- records/models.py
+++ records/models.py
@@ -31,6 +31,8 @@ class Records(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_new_release = models.BooleanField(default=False, verbose_name="Is New Release")
+    is_deal = models.BooleanField(default=False, verbose_name="Is Deal")
+    is_clearance = models.BooleanField(default=False, verbose_name="Is Clearance")
 
     def __str__(self):
         return self.name
--- records/views.py
+++ records/views.py
@@ -9,24 +9,35 @@ def all_records(request):
 
     records = Records.objects.all()
     query = ''
+    category = request.GET.get('category', '')
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q'].strip()
-            if not query:
+            if not query and not category:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('records'))
             
-            queries = (
-                Q(name__icontains=query) |
-                Q(description__icontains=query) |
-                Q(category__name__icontains=query)
-            )
-            records = records.filter(queries).distinct()
+            if query:
+                if query == 'is_new_release,deals':
+                    records = records.filter(Q(is_new_release=True) | Q(is_deal=True))
+                elif query == 'is_new_release':
+                    records = records.filter(is_new_release=True)
+                elif query == 'deals':
+                    records = records.filter(is_deal=True)
+                else:
+                    queries = (
+                        Q(name__icontains=query) |
+                        Q(description__icontains=query) |
+                        Q(category__name__icontains=query)
+                    )
+                    records = records.filter(queries).distinct()
+        elif category:
+            records = records.filter(category__name__icontains=category).distinct()
 
     context = {
         'records': records,
-        'search_term': query,
+        'search_term': query or category,
     }
 
     return render(request, 'records/records.html', context)
@@ -41,3 +52,4 @@ def record_detail(request, record_id):
     }
 
     return render(request, 'records/record_detail.html', context)
+
