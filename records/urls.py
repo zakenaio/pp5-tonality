6ea955d2a70bd4bd83b4ebd2a6917e2a6d6fb614
Anders Miller <anders.miller@gmail.com>
2024-03-12 13:09:10 +0000

    Fixed path

--- records/urls.py
+++ records/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Records, name='records'),
+    path('', views.all_records, name='records'),
     path('<int:record_id>/', views.record_detail, name='record_detail'),
     path('add/', views.add_record, name='add_record'),
     path('edit/<int:record_id>/', views.edit_record, name='edit_record'),
