212cf86f4e4f6a9efdef59d9cf610db7e1133db7
Anders Miller <anders.miller@gmail.com>
2024-03-12 13:30:31 +0100

    Adding crud for admin, having issues

--- records/urls.py
+++ records/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_records, name='records'),
-    path('<record_id>', views.record_detail, name='record_detail'),
+    path('', views.Records, name='records'),
+    path('<int:record_id>/', views.record_detail, name='record_detail'),
+    path('add/', views.add_record, name='add_record'),
+    path('edit/<int:record_id>/', views.edit_record, name='edit_record'),
+    path('delete/<int:record_id>/', views.delete_record, name='delete_record'),
 ]
-
